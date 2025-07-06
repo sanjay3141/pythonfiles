39ba96bc8f1a18c814d7386e70c5cf6116f4a8c4
sanjaysivakumar <sanjaysivakumar@gmail.com>
2025-07-06 08:31:06 -0600

    second commit

---
+++ simple_py_without_image.py
@@ -0,0 +1,31 @@
+x=int(input("Enter the first valve:"))
+y=int(input("Enter the second valve:"))
+z=x+y
+t=x-y
+h=x*y
+j=x/y
+print("the sum is:",z)
+print("the difference is:",t)
+print("the mutipliyed is:",h)
+print("the divided valve is:",j)
+g,l=2,6
+g,l=l,g+2
+print(g,l)
+print(type(j))
+b=(10+3==13)
+print(b)
+k=int(input("Enter the first valve:"))
+if(k==9):
+    print("k=",k, "is even")
+else:
+    print ("its odd:")
+o=int(input("enter the valve:"))
+i=int(input("enter the valve:"))
+t=o
+o=i
+i=t
+print(o,i)
+
+
+
+
