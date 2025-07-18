862d56800c610e42ba12d4bbffd76d7954d40a92
sanjaysivakumar <sanjaysivakumar@gmail.com>
2025-07-18 08:56:08 -0600

    fourth commit

---
+++ for_looping_1.py
@@ -0,0 +1,9 @@
+print("*******This Function runs without range********")
+print("****** sequence of odd number*******")
+# this fuction describes looping
+def var():
+   m = ["1", "3", "5", "7", "9", "....","n+2"]
+   for x in m :
+     print(x,end=" ")
+var()    
+
