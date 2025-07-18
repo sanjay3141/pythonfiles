6a8b60511c8405e06d10d393365ef8e343bf3723
sanjaysivakumar <sanjaysivakumar@gmail.com>
2025-07-18 09:07:11 -0600

    fifth commit

---
+++ for_looping_2.py
@@ -0,0 +1,14 @@
+# THIS FUNCTION DESCRIBES FOR LOOPING USING RANGE
+print("******this programm is to add successive number by 2******")
+def get():
+    for i in range(0,11,2):
+        print (i)
+get()        
+
+
+# THIS FUNCTION DESCRIBES UPPER TO LOWER ORDER
+print("******this programm is to subract successive number by 2******")
+def get():
+    for i in range(11,0,-2):
+        print (i)
+get()
