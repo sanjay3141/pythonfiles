fab30e0523a5fa07d5354b7985306890249edf09
sanjaysivakumar <sanjaysivakumar@gmail.com>
2025-07-26 08:41:49 -0600

    eight commit

---
+++ reversing_alphabets.py
@@ -0,0 +1,7 @@
+print("*******THIS PROGRAMM IS TO REVERSE ALPHABETS IN A WORD*********")
+x=input("ENTER THE WORD TO BE REVERSED:")
+for i in range(-1,-len(x)-1,-1):
+    print(x[i])
+    "\ n"
+
+print("*********END**********"," \n")
